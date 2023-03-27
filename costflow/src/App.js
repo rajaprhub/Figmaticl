8e70d283a0e63188a90db405c3b15f2b4a530420
rajaprhub <98698646+rajaprhub@users.noreply.github.com>
2023-03-27 13:23:42 +0530

    flowapp

---
+++ costflow/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Boltfcost } from './Component/Boltfcost';
+
+function App() {
+  return (
+    <div className="App">
+         <h1> cost </h1>
+
+         <Boltfcost/>
+    </div>
+  );
+}
+
+export default App;
