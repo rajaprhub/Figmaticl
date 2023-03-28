666ce8b90f34593241d11aabb664cdd2727911a4
rajaprhub <98698646+rajaprhub@users.noreply.github.com>
2023-03-28 23:26:10 +0530

    bcolor

--- costflow/src/Component/Count.jsx
+++ costflow/src/Component/Count.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { useState,useEffect } from 'react' ;
 import '../Styles/Count.css' ;
-const resobj = { 1 : "None" , 2 : "Basic", 3 : "Advance", 4 : "Complex" }
+
+const resobj =  { 1 : {name: "None" , bgcolor :"whitesmoke"},
+                  2 : {name: "Basic" , bgcolor :"#91D4A8"},
+                  3 : {name: "Advance" , bgcolor :"#E9BE74"},
+                  4 : {name: "Complex" , bgcolor :"#F57F6C"},
+               }
 
 export const Count = () => {
   const [count,setCount] = useState(1) ;
   const reduce = ()=>{ setCount(count - 1) }
   const add    = ()=>{ setCount(count + 1) }
-  const [name, setName] = useState('None');
 
+ 
   return (
     <> 
            <div  className='btnsdiv'>  
-            <button className='minusbtn'  disabled = {count == 1} onClick={reduce}> - </button>
+            <button  className='minusbtn'  disabled = {count == 1} onClick={reduce}> - </button>
              <section className='nbacname'>
-               <span>{ resobj[count]} </span>
+               <span style={ {backgroundColor: `${ resobj[count].bgcolor}`}}> {resobj[count].name} </span>
              </section>
             <button className='plusbtn' disabled = {count == 4}  onClick={add}> + </button>
          </div>
